799a5e766430b468b5c3e9ae2005f6eb7d2dee89
Vincent Koester <vincent.koester@gmx.de>
2019-05-29 12:58:59 -0300

    Solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_29_150914) do
+ActiveRecord::Schema.define(version: 2019_05_29_151127) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.integer "stars"
+    t.string "address"
+    t.string "phone_number"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
